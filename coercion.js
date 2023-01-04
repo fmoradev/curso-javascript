205475f2a3bbc7857b20762a75d4ce9d85c8d6b7
Francisco Mora <fr.morac@duocuc.cl>
2023-01-04 13:16:44 -0300

    Added coercion.js and hoisting.js

---
+++ coercion.js
@@ -0,0 +1,11 @@
+//implicita
+4 + "7" // 47
+4 * "7" // 28
+2 + true // 3
+false - 3 // -3
+!3 // false
+
+// explicita
+Number("47") // 'number'
+String(51) // 'string'
+Boolean(1) // 'boolean'
---
+++ hoisting.js
@@ -0,0 +1,8 @@
+var nombre;
+nombre = "John";
+
+hey();
+
+function hey(){
+    console.log("Hola " + miNombre); //undefined
+}
