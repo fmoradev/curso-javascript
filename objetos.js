cd7df0a7cf67dc2d18301e47843a4eef561b6921
Francisco Mora <fr.morac@duocuc.cl>
2023-02-10 13:23:12 -0300

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -1,10 +1,25 @@
 var miAuto = {
     marca: "Totota",
     modelo: "Corolla",
-    annio: 2017
+    annio: 2017,
+    detalleAuto: function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
 };
 
 miAuto;
 miAuto.marca;
 miAuto.modelo;
 miAuto.annio;
+miAuto.detalleAuto();
+
+function Auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+    this.detalleAuto = function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
+}
+
+var miAuto = new Auto("Toyota", "Corolla", 2020);
